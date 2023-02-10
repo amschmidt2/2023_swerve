9d45b095a04a9e58e73a056412ad8797f12a3de6
sweeneyb <tab126@gmail.com>
2023-02-10 16:14:30 -0500

    fix compressor commands to properly use the subsystem

--- src/main/java/frc/robot/commands/CompressorCommandExtend.java
+++ src/main/java/frc/robot/commands/CompressorCommandExtend.java
@@ -2,31 +2,25 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-//package frc.robot.commands;
+package frc.robot.commands;
 
-//import edu.wpi.first.wpilibj.DoubleSolenoid;
-//import edu.wpi.first.wpilibj.PneumaticsModuleType;
-///import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
-//import frc.robot.subsystems.Compressor;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Compressor;
 
-//public class CompressorCommandExtend extends CommandBase {
+public class CompressorCommandExtend extends CommandBase {
   /** Creates a new Compressor. */
-  //DoubleSolenoid leftDS = new DoubleSolenoid((PneumaticsModuleType.CTREPCM), 0, 1); //object
-  //DoubleSolenoid rightDS = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 3, 4);
 
-  //Compressor compressor;
+  Compressor compressor;
 
-  //public CompressorCommandExtend(Compressor compressor ) {
-   // this.compressor = compressor;
-  //}
-  //@Override
-  //public void execute() {
-    //System.out.println("Executing "+ this.getClass().getName());
-   // compressor.forward();
- // }
+  public CompressorCommandExtend(Compressor compressor) {
+    addRequirements(compressor);
+    this.compressor = compressor;
+  }
 
+  // @Override
+  public void execute() {
+    // System.out.println("Executing "+ this.getClass().getName());
+    compressor.forward();
+  }
 
-
-//} // <-- leave this bracket 
-
-
+} // <-- leave this bracket
