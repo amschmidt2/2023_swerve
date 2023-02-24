233e00d852b37f1083a96df5b04c0f6761b57d3a
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2023-02-24 15:38:10 -0600

    more button

---
+++ src/main/java/frc/robot/commands/armPositions/ArmHumanCubeCommand.java
@@ -0,0 +1,17 @@
+package frc.robot.commands.armPositions;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
+
+public class ArmHumanCubeCommand extends CommandBase{
+    Arm arm;
+
+    public ArmHumanCubeCommand(Arm arm){
+        addRequirements(arm);
+        this.arm = arm;
+    }
+
+    public void execute(){
+        arm.armHumanCube();
+    }
+
+} // <-- keep brace 
