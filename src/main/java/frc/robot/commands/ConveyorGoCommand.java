da3c2ab9d3c556a57ce4f8c6ebe234f0284ce2cd
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2023-02-24 15:42:18 -0600

    Create ConveyorGoCommand.java

---
+++ src/main/java/frc/robot/commands/ConveyorGoCommand.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Conveyor;
+
+public class ConveyorGoCommand extends CommandBase{
+    Conveyor conveyor;
+
+    public ConveyorGoCommand(Conveyor conveyor){
+        addRequirements(conveyor);
+        this.conveyor = conveyor;
+    }
+
+    public void execute(){
+        conveyor.go();
+    }
+
+} // <-- kep brace 
