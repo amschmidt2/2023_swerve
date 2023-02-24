ca3cd46f58d681caa97d6f62d0ec1386ac49791a
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2023-02-24 15:43:24 -0600

    Create ConveyorStopCommand.java

---
+++ src/main/java/frc/robot/commands/ConveyorStopCommand.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Conveyor;
+
+
+public class ConveyorStopCommand extends CommandBase {
+    Conveyor conveyor;
+
+    public ConveyorStopCommand(Conveyor conveyor){
+        addRequirements(conveyor);
+        this.conveyor = conveyor;
+    }
+
+    public void execute(){
+        conveyor.stop();
+    }
+
+
+
+
+} // <-- kep brace 
