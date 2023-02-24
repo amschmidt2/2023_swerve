8cf266be67489f1dcc33550b1cdbbf721fd86c9d
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2023-02-24 15:49:15 -0600

    done w/ buttons :)

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -12,7 +12,8 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Conveyor extends SubsystemBase{
 
-
+    private CANSparkMax conveyorMotor = new CANSparkMax(13, MotorType.kBrushless); 
+    private double speed = 0.6;
 
     public Conveyor(){
 
