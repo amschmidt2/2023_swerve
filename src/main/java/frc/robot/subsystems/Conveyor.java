e78aa59e215d7571c1f4c757dda6c25c7a23eae0
aebill14 <97410781+aebill14@users.noreply.github.com>
2023-02-24 20:08:42 -0600

    made conveyor speed higher

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -6,7 +6,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class Conveyor extends SubsystemBase{
 
     private CANSparkMax conveyorMotor = new CANSparkMax(13, MotorType.kBrushless); 
-    private double speed = 0.6;
+    private double speed = 0.45;
 
     public Conveyor(){
 
