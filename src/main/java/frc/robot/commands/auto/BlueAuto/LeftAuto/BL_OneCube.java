999d5b4df3c995fb905367c5435f5070e57a3012
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2023-03-07 18:55:14 -0600

    maybe swerve moves :)
    Needs more work tho

--- src/main/java/frc/robot/commands/auto/BlueAuto/LeftAuto/BL_OneCube.java
+++ src/main/java/frc/robot/commands/auto/BlueAuto/LeftAuto/BL_OneCube.java
@@ -1,4 +1,5 @@
 package frc.robot.commands.auto.BlueAuto.LeftAuto;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.armPositions.ArmConeHighCommand;
 import frc.robot.commands.armPositions.ArmCubeHighCommand;
@@ -13,48 +14,53 @@ import frc.robot.commands.conveyor.ConveyorGoCommand;
 import frc.robot.commands.conveyor.ConveyorStopCommand;
 import frc.robot.commands.floorIntake.FloorIntakeCollectCommand;
 import frc.robot.commands.floorIntake.FloorIntakeStopCommand;
+import frc.robot.commands.swerve.MoveSwervePos;
+import frc.robot.commands.swerve.SetSwerveDrive;
 import frc.robot.commands.swerve.SetSwerveIdleMode;
+import frc.robot.commands.swerve.SetSwerveOdometry;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.IntakeArm;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Compressor;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.FloorIntake;
- /*
- * throttle goes straight or back
- * strafe goes left rigth
- */
+/*
+* throttle goes straight or back
+* strafe goes left rigth
+*/
 
-public class BL_OneCube extends SequentialCommandGroup{
-    
+public class BL_OneCube extends SequentialCommandGroup {
+    /*
+     * 1. Arm High Drops Cone
+     * 2. Arm Down & AIntake Off
+     * 3. Swerve drives 60 meters
+     * 4. Compressor and FIntake start running
+     * 5. Collect Cube Conveyor and AIntake move
+     * 6. Stop eveything
+     * 7. Swerve move forward
+     * 8. Get to Cube station
+     */
+
     public BL_OneCube(DriveSubsystem swerveDrive, Arm arm, IntakeArm intakeArm, Compressor compressor, Conveyor conveyor, FloorIntake floorIntake){
         addCommands(
-            //Arm goes to high and drops cube
-            new ArmConeHighCommand(arm).andThen(new IntakeArmConeExtractCommand(intakeArm)),
-            //IntakeArm stops and Arm drops, this goes at end
-            new IntakeArmStopCommand(intakeArm).andThen(new ArmGoDown(arm)),
-            //Robot Moves back
-            new SetSwerveIdleMode(swerveDrive, true).andThen(() -> swerveDrive.drive(-0.5, 0, 0, false)),
-            //Compress out and FloorIntake Munch, then wheels stop
-            new CompressorCommandExtend(compressor).andThen(new FloorIntakeCollectCommand(floorIntake)).
-            andThen(new SetSwerveIdleMode(swerveDrive, false).andThen(() -> swerveDrive.drive(0, 0, 0, false))),
-            //Compress In and FloorIntake stop
-            new CompressorCommandRetract(compressor).andThen(new FloorIntakeStopCommand(floorIntake)),
-            //Convey go and IntakeArm go
-            new ConveyorGoCommand(conveyor).andThen(new IntakeArmCubeCommand(intakeArm)),
-            // Convey Stop and IntakeArm stop
-            new ConveyorStopCommand(conveyor).andThen(new IntakeArmStopCommand(intakeArm)),
-            //Robot Moves Forward
-            new SetSwerveIdleMode(swerveDrive, true).andThen(() -> swerveDrive.drive(0.5, 0, 0, false)).     
-            andThen(new SetSwerveIdleMode(swerveDrive, false).andThen(() -> swerveDrive.drive(0, 0, 0, false))),
-            //Robot Moves Right
-            new SetSwerveIdleMode(swerveDrive, true).andThen(() -> swerveDrive.drive(0, 0.3, 0, false)).
-            andThen(new SetSwerveIdleMode(swerveDrive, false).andThen(() -> swerveDrive.drive(0, 0, 0, false))),
-            //Cube High 
-            new ArmCubeHighCommand(arm).andThen(new IntakeArmCubeExtractCommand(intakeArm)),
-            //Arm Down
-            new IntakeArmStopCommand(intakeArm).andThen(new ArmGoDown(arm))
-            // ~Fin~
+
+            new ArmConeHighCommand(arm).andThen(new IntakeArmConeExtractCommand(intakeArm)).
+
+            andThen(new IntakeArmStopCommand(intakeArm)).andThen(new ArmGoDown(arm)).
+
+            andThen(new MoveSwervePos(swerveDrive))
+
+            // new CompressorCommandExtend(compressor).andThen(new FloorIntakeCollectCommand(floorIntake)).
+            // andThen(new SetSwerveIdleMode(swerveDrive, false).andThen(() -> swerveDrive.drive(0, 0, 0, false))),
+            // new CompressorCommandRetract(compressor).andThen(new FloorIntakeStopCommand(floorIntake)),
+            // new ConveyorGoCommand(conveyor).andThen(new IntakeArmCubeCommand(intakeArm)),
+            // new ConveyorStopCommand(conveyor).andThen(new IntakeArmStopCommand(intakeArm)),
+            // new SetSwerveIdleMode(swerveDrive, true).andThen(() -> swerveDrive.drive(0.5, 0, 0, false)).     
+            // andThen(new SetSwerveIdleMode(swerveDrive, false).andThen(() -> swerveDrive.drive(0, 0, 0, false))),
+            // new SetSwerveIdleMode(swerveDrive, true).andThen(() -> swerveDrive.drive(0, 0.3, 0, false)).
+            // andThen(new SetSwerveIdleMode(swerveDrive, false).andThen(() -> swerveDrive.drive(0, 0, 0, false))),
+            // new ArmCubeHighCommand(arm).andThen(new IntakeArmCubeExtractCommand(intakeArm)),
+            // new IntakeArmStopCommand(intakeArm).andThen(new ArmGoDown(arm))
 
         );
 
@@ -62,5 +68,4 @@ public class BL_OneCube extends SequentialCommandGroup{
 
     }
 
-
-} //<--
+} // <--
---
+++ src/main/java/frc/robot/commands/swerve/MoveSwervePos.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.swerve;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class MoveSwervePos extends CommandBase{
+   
+    private DriveSubsystem m_drive;
+
+    public MoveSwervePos(DriveSubsystem m_drive){
+        this.m_drive = m_drive;
+        //this.m_mp = m_mp;
+        addRequirements(m_drive);
+    }
+
+    @Override
+    public void execute(){
+        m_drive.drive(0.25, 0, 0, false);
+    }
+    
+    public boolean isFinished(){
+        if(m_drive.getPoseMeters().getX() < 1){
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+} // <--
